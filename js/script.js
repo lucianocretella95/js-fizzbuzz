f29f8c446c76c634aad9241572d9972336ed40c0
lucianocretella95 <luciano.cretella@tim.it>
2024-04-19 17:03:01 +0200

    list in pagina

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 for (let number = 1; number <= 100; number++) {
+  const li = document.querySelector("li");
+  const element = `<li class="box box--${number}">${number}</li>`;
+  li.innerHTML += element;
   if (number % 3 == 0 && number % 5 == 0) {
-    console.log(number , "FizzBuzz");
+    console.log(number, "FizzBuzz");
   } else if (number % 3 == 0) {
-    console.log(number , "Fizz");
+    console.log(number, "Fizz");
   } else if (number % 5 == 0) {
-    console.log(number , "Buzz");
+    console.log(number, "Buzz");
   } else {
     console.log(number);
   }
