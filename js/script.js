aa7dc5fc711a1b87dbedcaedf7a0a9d4db5cf231
lucianocretella95 <luciano.cretella@tim.it>
2024-04-22 10:38:31 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ for (let number = 1; number <= 100; number++) {
   const element = `<li class="box box--${number}">${number}</li>`;
   li.innerHTML += element;
   if (number % 3 == 0 && number % 5 == 0) {
-    console.log(number, "FizzBuzz");
+    // console.log(number, "FizzBuzz");
     li.append("FizzBuzz");
   } else if (number % 3 == 0) {
-    console.log(number, "Fizz");
+    // console.log(number, "Fizz");
     li.append("Fizz");
   } else if (number % 5 == 0) {
-    console.log(number, "Buzz");
+    // console.log(number, "Buzz");
     li.append("Buzz")
   } else {
-    console.log(number);
+    li.append("");
   }
 }
